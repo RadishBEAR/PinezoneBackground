4c991673a2fc116a4335462243167353b3fc1355
NoobNoobHui <50476469+NoobNoobHui@users.noreply.github.com>
2020-05-06 18:09:29 +0800

    Update ArticalDataCard.vue

--- demo/src/components/MainContainer/ArticalData/ArticalDataCard.vue
+++ demo/src/components/MainContainer/ArticalData/ArticalDataCard.vue
@@ -1,8 +1,33 @@
 <template>
     <div id="articaDataCard">
         <div style="width: 100%;height: 30px;">
-            <p style="color: #5d4037;margin-top: 5px;margin-left: 5px;">
-                {{"文章名："+this.articalName}}
+            <p style="color: #5d4037;margin-top: 20px;margin-left: 5px;font-size: 18px">
+                {{"文章名："+this.articalName1}}
+            </p>
+        </div>
+        <div style="width: 100%;height: 30px;">
+            <p style="color: #5d4037;margin-top: 25px;margin-left: 5px;font-size: 18px">
+                {{"文章名："+this.articalName2}}
+            </p>
+        </div>
+        <div style="width: 100%;height: 30px;">
+            <p style="color: #5d4037;margin-top: 25px;margin-left: 5px;font-size: 18px">
+                {{"文章名："+this.articalName3}}
+            </p>
+        </div>
+        <div style="width: 100%;height: 30px;">
+            <p style="color: #5d4037;margin-top: 25px;margin-left: 5px;font-size: 18px">
+                {{"文章名："+this.articalName4}}
+            </p>
+        </div>
+        <div style="width: 100%;height: 30px;">
+            <p style="color: #5d4037;margin-top: 25px;margin-left: 5px;font-size: 18px">
+                {{"文章名："+this.articalName5}}
+            </p>
+        </div>
+        <div style="width: 100%;height: 30px;">
+            <p style="color: #5d4037;margin-top: 25px;margin-left: 5px;font-size: 18px">
+                {{"文章名："+this.articalName6}}
             </p>
         </div>
     </div>
@@ -12,7 +37,12 @@
     export default {
         name: "ArticalDataCard",
         props:{
-            articalName: String
+            articalName1: String,
+            articalName2: String,
+            articalName3: String,
+            articalName4: String,
+            articalName5: String,
+            articalName6: String,
         }
     }
 </script>>
@@ -21,6 +51,6 @@
     #articalDataCard{
         width: 70%;
         margin-left: 50px;
-        margin-top: 30px;
+        margin-top: 50px;
     }
 </style>
